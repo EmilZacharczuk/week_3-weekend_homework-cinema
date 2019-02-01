b8f3572860bd36c7a538541957800e386da45221
EmilZacharczuk <emil.zacharczuk@yahoo.pl>
2019-02-01 13:40:23 +0000

    Initial setup

--- console.rb
+++ console.rb
@@ -1,23 +1,20 @@
 require('pry')
 
-require_relative('..models/custumer')
-require_relative('../models/film')
-require_relative('../models/ticket')
+require_relative('models/customer.rb')
+require_relative('models/film.rb')
+require_relative('models/ticket.rb')
 
 customer_1 = Customer.new({
   'name' => 'Emil',
-  'funds' => 50
-})
+  'funds' => 50})
 
 customer_2 = Customer.new({
-  'name' => 'Mark'
-  'funds' => 30
-  })
+  'name' => 'Mark',
+  'funds' => 30})
 
 customer_3 = Customer.new({
-  'name' => 'Anna'
-  'funds' => 20
-  })
+  'name' => 'Anna',
+  'funds' => 20})
 
 binding.pry
 nil
--- models/film.rb
+++ models/film.rb
@@ -1,10 +1,10 @@
-require_relative('..db/sql_runner.rb')
+require_relative('../db/sql_runner.rb')
 
 class Film
 
   attr_reader :id
   attr_accessor :title, :price
-  
+
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @title = options['title']
